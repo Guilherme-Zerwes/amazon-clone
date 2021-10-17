572cef6f09ae556806fc363a7857fee2e8ded680
Guilherme Zerwes <gui.zerwes@gmail.com>
2021-10-16 23:16:32 -0300

    app built

---
+++ src/components/Header.js
@@ -0,0 +1,48 @@
+import Image from 'next/image'
+import {ShoppingCartIcon,MenuIcon,SearchIcon} from '@heroicons/react/outline'
+
+function Header() {
+    return (
+        <header>
+            <div className="flex items-center bg-amazon_blue p1 flex-grow py-2">
+                <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+                    <Image src="https://links.papareact.com/f90" width={150} height={40} objectFit="contain"
+                        className="cursor-pointer"/>
+                </div>
+                <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500 transition duration-200">
+                    <input type="text" className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none px-4"/>
+                    <SearchIcon className="h-12 p-4"/>
+                </div>
+                <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+                    <div className="cursor-pointer link">
+                        <p>Hello User</p>
+                        <p className="font-extrabold md:text-sm">Account & Lists</p>
+                    </div>
+                    <div className="cursor-pointer link">
+                        <p>Returns</p>
+                        <p className="font-extrabold md:text-sm">& Orders</p>
+                    </div>
+                    <div className="relative cursor-pointer link flex items-center">
+                        <span className="absolute top-0 right-0 md:right-10 h-4 w-4 text-center rounded-full font-extrabold bg-yellow-500">0</span>
+                        <ShoppingCartIcon className="h-10"/>
+                        <p className="hidden md:inline font-extrabold md:text-sm mt-2">Basket</p>
+                    </div>
+                </div>
+            </div>
+            <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white">
+                <p className="link flex items-center"><MenuIcon className="h-6 mr-1"/> All</p>
+                <p className="link">Prime Video</p>
+                <p className="link">Amazon Business</p>
+                <p className="link">Today's Deals</p>
+                <p className="hidden link lg:inline-flex">Electronics</p>
+                <p className="hidden link lg:inline-flex">Food & Grocery</p>
+                <p className="hidden link lg:inline-flex">Prime</p>
+                <p className="hidden link lg:inline-flex">Buy Again</p>
+                <p className="hidden link lg:inline-flex">Shopper Toolkit</p>
+                <p className="hidden link lg:inline-flex">Health & Personal Care</p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Product.js
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+import { useState } from 'react'
+import { StarIcon } from '@heroicons/react/solid'
+import Currency from 'react-currency-formatter'
+
+const Product = ({id,title,price,description,category,image}) => {
+    const [rating] = useState(Math.floor(Math.random()*5 + 1))
+    const [hasPrime] = useState(Math.random() < 0.5)
+    return (
+        <div className="relative flex-col m-5 bg-white z-30 p-10">
+            <p className="absolute top-2 right-2 text-xs italic text-gray-400">{category}</p>
+            <Image src={image} height={200} width={200} objectFit="contain"/>
+            <h4 className="my-3">{title}</h4>
+            <div className="flex">
+                {Array(rating).fill().map((_,i) => (
+                    <StarIcon className="h-5 text-yellow-500"/>
+                ))}
+            </div>
+            <p className="text-xs my-2 line-clamp-2">{description}</p>
+            <div className="mb-5">
+                <Currency quantity={price} currency="BRL"/>
+            </div>
+            {hasPrime && (
+                <div className="flex items-center space-x-2 -mt-5">
+                    <img className="w-12" src="https://links.papareact.com/fdw"/>
+                    <p className="text-xs text-gray-500">FREE Next-day delivery</p>
+                </div>
+            )}
+
+            <button className="mt-auto button">Add to Basket</button>
+        </div>
+    )
+}
+
+export default Product
