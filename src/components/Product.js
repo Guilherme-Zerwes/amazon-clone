0c021d04a6597069acf5ffe72f113379f23de289
Guilherme Zerwes <gui.zerwes@gmail.com>
2021-10-19 10:07:23 -0300

    fixed session bug

--- src/components/Product.js
+++ src/components/Product.js
@@ -33,7 +33,7 @@ const Product = ({id,title,price,description,category,image}) => {
             <h4 className="my-3">{title}</h4>
             <div className="flex">
                 {Array(rating).fill().map((_,i) => (
-                    <StarIcon className="h-5 text-yellow-500"/>
+                    <StarIcon key={i} className="h-5 text-yellow-500"/>
                 ))}
             </div>
             <p className="text-xs my-2 line-clamp-2">{description}</p>
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -4,35 +4,37 @@ import Image from 'next/image'
 import Currency from "react-currency-formatter"
 import { selectItems, selectTotal } from "../slices/basketSlice"
 import { useSelector } from "react-redux"
-import { session } from "next-auth/client"
+import { session, useSession } from "next-auth/client"
 
 const Checkout = () => {
     const items = useSelector(selectItems)
     const total = useSelector(selectTotal)
+    const [session] = useSession()
     return (
         <div className="bg-gray-100">
             <Header/>
-            <main className="lg:flex max-w-2xl mx-auto">
+            <main className="lg:flex max-w-3xl mx-auto">
                 <div className="flex-grow m-5 shadow-sm">
                     <Image src="https://links.papareact.com/ikj" width={1020} height={250} object="contain"/>
                     <div className="flex flex-col p-5 space-y-10 bg-white">
-                        <p className="text-3xl border-b pb-4">{items.length < 1 ? 'Sem itens, continue comprando' :
-                        items.map((item, i) => (
-                            <CheckoutProduct 
-                                key={i}
-                                id={item.id}
-                                title={item.title}
-                                price={item.price}
-                                rating={item.rating}
-                                description={item.description}
-                                category={item.category}
-                                hasPrime={item.hasPrime}
-                                image={item.image}
-                            />
-                        ))}</p>
+                        {items.length < 1 ? (<p className="text-3xl border-b pb-4">'Sem itens, continue comprando'</p>) :
+                            items.map((item, i) => (
+                                <CheckoutProduct 
+                                    key={i}
+                                    id={item.id}
+                                    title={item.title}
+                                    price={item.price}
+                                    rating={item.rating}
+                                    description={item.description}
+                                    category={item.category}
+                                    hasPrime={item.hasPrime}
+                                    image={item.image}
+                                />
+                            ))
+                        }
                     </div>
                 </div>
-                <div className="flex flex-col bg-white p-10 shadow-md">
+                <div className="flex flex-col mt-5 bg-white p-10 shadow-md">
                     {items.length > 0 && (
                         <>
                             <h2 className="whitespace-nowrap">
